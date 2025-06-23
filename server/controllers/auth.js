85400dcbf00bfa86a2ea29d2598894ff47e4f946
M.A.C. Jayasankha <charithjayasankha@gmail.com>
2025-06-23 19:12:04 +0530

    Update auth.js

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -36,7 +36,7 @@ export const register = async (req, res) => {
     res.status(201).json({ user:userWithoutPassword,token });
   } catch (error) {
     console.error("Error registering user:", error);
-    res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({ message: error.message || "Internal server error" });
   }
 };
 
