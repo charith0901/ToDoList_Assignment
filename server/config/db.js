723db211808cb48f5769403a5e82e6719c9715c0
M.A.C. Jayasankha <charithjayasankha@gmail.com>
2025-06-23 19:33:10 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,8 +2,15 @@ import mongoose from 'mongoose';
 mongoose.set('strictQuery', true);
 
 const connect = () => {
-    return mongoose.connect(process.env.MONGODB_URI);
-    
-};
-
+    try {
+        mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected successfully');
+    } catch (error) {
+        console.error('MongoDB connection failed:', error);
+        throw new Error('MongoDB connection failed');
+    }   
+}
 export default connect;
